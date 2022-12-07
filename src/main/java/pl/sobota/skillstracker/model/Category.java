0b3f47d9dea17f4d7f9fac7547967a8f635e1fc0
sobotapl <piosoboci@gmail.com>
2022-12-07 19:10:27 +0100

    CategoryRepository updated

--- src/main/java/pl/sobota/skillstracker/model/Category.java
+++ src/main/java/pl/sobota/skillstracker/model/Category.java
@@ -1,11 +1,17 @@
 package pl.sobota.skillstracker.model;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 import javax.persistence.*;
 
 @Data
 @Entity
 @Table(name = "categories")
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
 public class Category {
 
     @Id
--- src/main/java/pl/sobota/skillstracker/repository/CategoryRepository.java
+++ src/main/java/pl/sobota/skillstracker/repository/CategoryRepository.java
@@ -3,7 +3,10 @@ package pl.sobota.skillstracker.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.sobota.skillstracker.model.Category;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
 
+    Optional<Category> findByFirstNameAndDescription(String name, String description);
 }
